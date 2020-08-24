4f27a86ebed1725a05baa8c8945fb8998bbf0c91
Tanveer Ahamed <tanveer1.mtm@gmail.com>
2020-08-24 12:17:44 +0530

    initial react app

---
+++ src/component/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import "../css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
