eb4442ef2f6dfc10ffe793d69dc76783de7e0c8e
Tanveer Ahamed <tanveer1.mtm@gmail.com>
2020-08-24 15:55:51 +0530

    header component added

--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import Header from "./Header";
 import logo from "../images/logo.svg";
 import "../css/App.css";
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,59 @@
-import react from "react";
+import React from "react";
+import photo from "../images/EDIT.jpg";
+import "../css/Header.css";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="header">
+      <div className="header-nav">
+        <div className="header-photo-continer d">
+          <img className="header-photo" src={photo} alt="profilepic" />
+        </div>
+        <div className="header-text d">
+          <h1>
+            <span className="">H</span>
+            <span className="">i</span>
+            <span className="">,</span>
+
+            <br />
+            <span className="">I</span>
+            <span className="">'</span>
+            <span className="">m</span>
+            <span className="name f">T</span>
+            <span className="name">a</span>
+            <span className="name">n</span>
+            <span className="name">v</span>
+            <span className="name">e</span>
+            <span className="name">e</span>
+            <span className="name">r</span>
+            <span className="name f">A</span>
+            <span className="name">h</span>
+            <span className="name">a</span>
+            <span className="name">m</span>
+            <span className="name">e</span>
+            <span className="name">d</span>
+          </h1>
+        </div>
+      </div>
+
+      <div className="header-links">
+        <ul className="header-lists">
+          <li className="list-item">
+            <a href="$">Education</a>
+          </li>
+          <li className="list-item">
+            <a href="$">Projects</a>
+          </li>
+          <li className="list-item">
+            <a href="$">Writing</a>
+          </li>
+          <li className="list-item">
+            <a href="$">Contact me</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
+
+export default Header;
