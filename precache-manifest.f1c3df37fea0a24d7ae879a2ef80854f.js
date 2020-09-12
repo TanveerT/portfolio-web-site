126856464d11bb489c2fcff2e1f8baac133c09eb
Tanveer Ahamed <tanveer1.mtm@gmail.com>
2020-09-12 20:42:03 +0530

    Updates

---
+++ precache-manifest.f1c3df37fea0a24d7ae879a2ef80854f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8df6f9cc9f1f1876934765644050a7",
+    "url": "/portfolio-web-site/index.html"
+  },
+  {
+    "revision": "6618811a22f78d11eb60",
+    "url": "/portfolio-web-site/static/css/main.90bd083a.chunk.css"
+  },
+  {
+    "revision": "6a9ef3738f5ca902f575",
+    "url": "/portfolio-web-site/static/js/2.933173bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-web-site/static/js/2.933173bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6618811a22f78d11eb60",
+    "url": "/portfolio-web-site/static/js/main.78d6497c.chunk.js"
+  },
+  {
+    "revision": "a6bd797906732238c111",
+    "url": "/portfolio-web-site/static/js/runtime-main.cd05ee6e.js"
+  },
+  {
+    "revision": "051843db15b4d1ba92db55377102f3f9",
+    "url": "/portfolio-web-site/static/media/EDIT-min.051843db.jpg"
+  }
+]);
